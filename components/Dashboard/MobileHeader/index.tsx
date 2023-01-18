f1a100989cd0074bb41154a90eb216116c9de321
Jonathan <jonathancarpena99@gmail.com>
2023-01-17 17:03:36 -0800

    Styled Sidebar for tablet screens

--- components/Dashboard/MobileHeader/index.tsx
+++ components/Dashboard/MobileHeader/index.tsx
@@ -2,7 +2,7 @@ import { BsSpotify } from 'react-icons/bs';
 
 function MobileHeader() {
 	return (
-		<h1 className="drop-shadow-md z-30 select-none  text-white  space-x-1.5 items-center flex fixed top-3 left-3     md:hidden ">
+		<h1 className="drop-shadow-md z-30 select-none  text-white   space-x-1.5 items-center flex fixed top-3 left-3     md:hidden ">
 			<BsSpotify className="text-3xl " />
 			<span className="text-xl font-bold tracking-tight">Swaddle</span>
 		</h1>
--- components/Dashboard/Sidebar/SidebarItem.tsx
+++ components/Dashboard/Sidebar/SidebarItem.tsx
@@ -19,7 +19,7 @@ function SidebarItem({
 	return (
 		<li
 			onClick={() => handleMenuChange(value)}
-			className={`active:scale-90 select-none cursor-pointer p-1 flex flex-col   items-center  hover:text-black dark:hover:text-white transition-all duration-200 w-full ${
+			className={`active:scale-90 select-none cursor-pointer lg:py-1 flex flex-col   items-center  hover:text-black dark:hover:text-white transition-all duration-200 w-full ${
 				menu === value
 					? 'text-black dark:text-white'
 					: 'text-light-secondary dark:text-dark-secondary'
@@ -28,7 +28,7 @@ function SidebarItem({
 			{menu === value ? active : inactive}
 
 			<span
-				className={`text-center md:text-start text-xs md:text-sm flex-1  font-bold capitalize `}
+				className={`text-center md:text-start text-xs  lg:text-sm flex-1  font-bold capitalize `}
 			>
 				{value}
 			</span>
