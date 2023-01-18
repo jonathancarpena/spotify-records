33b21fbec5dae81bac5264b8de73a7fffe5e510d
Jonathan <jonathancarpena99@gmail.com>
2023-01-18 12:15:37 -0800

    Styled Header for tablet screens

--- components/Dashboard/Header.tsx
+++ components/Dashboard/Header.tsx
@@ -46,10 +46,10 @@ function ListOfTerms({ term, setTerm }: ListOfTermsProps) {
 		setTerm(value);
 	}
 	return (
-		<ul className=" text-white z-30 w-max  flex items-center justify-center space-x-2 md:text-base text-sm">
+		<ul className=" text-white z-30 w-max  flex items-center justify-center space-x-2 lg:text-base text-sm">
 			<button
 				disabled
-				className="font-semibold bg-white bg-opacity-25 rounded-sm md:rounded-md px-2"
+				className="font-semibold bg-white bg-opacity-25 rounded-sm lg:rounded-md px-2"
 			>
 				{termPlaceholders[active]}
 			</button>
@@ -60,7 +60,7 @@ function ListOfTerms({ term, setTerm }: ListOfTermsProps) {
 					<button
 						key={item.value}
 						onClick={() => handleButtonClick(item.value)}
-						className="font-light hover:underline "
+						className="font-light hover:underline  text-sm lg:text-base "
 					>
 						{`${item.placeholder}${idx === 0 ? ',' : ''}`}
 					</button>
@@ -71,14 +71,14 @@ function ListOfTerms({ term, setTerm }: ListOfTermsProps) {
 function Header({ term, setTerm, title, banner }: Props) {
 	return (
 		<header>
-			<div className=" flex flex-col justify-end px-5 pb-2 h-[18.5rem] md:h-[25.5rem] md:px-10 md:pt-10 md:pb-8 relative  bg-gradient-to-b from-accent-500">
+			<div className=" flex flex-col justify-end px-5 pb-2 h-[18.5rem] lg:h-[25.5rem] lg:px-10 lg:pt-10 md:pb-8 relative  bg-gradient-to-b from-accent-500">
 				<div
 					className={`absolute w-full h-[130%] md:h-[125%]  bg-cover bg-center left-0 top-0  z-20 ${banner}`}
 				>
 					<div className=" -z-10 w-full h-[100px] dark:h-[200px]  absolute bottom-0 bg-gradient-to-b from-transparent dark:to-dark-main to-light-main"></div>
 				</div>
 
-				<h2 className="text-5xl mb-4 text-white md:text-8xl font-black z-30 capitalize md:mb-8 drop-shadow-sm">
+				<h2 className="text-5xl mb-4 text-white md:text-6xl lg:text-8xl font-black z-30 capitalize md:mb-6 lg:mb-8 drop-shadow-sm">
 					{title}
 				</h2>
 
